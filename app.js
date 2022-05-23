ca923943c7701e6acf3e0b14822cd6045e8fafca
omermaim <omermaimon2@gmail.com>
2022-05-23 17:58:37 +0300

    with about but still with different css

--- app.js
+++ app.js
@@ -1192,12 +1192,33 @@ function showGame(){
 
 
 function openAbout() {
-	document.getElementById("About").showModal();
+
+	var modal = document.getElementById("About");
+	var span = document.getElementsByClassName("close")[0];
+	modal.style.display = "block";
+	span.onclick = function() {
+		modal.style.display = "none";
+	  }
+	  window.onclick = function(event) {
+		if (event.target == modal) {
+		  modal.style.display = "none";
+		}
+	  }
+	  $(document).keydown(function(event) { 
+		if (event.keyCode == 27) { 
+			modal.style.display="none";
+		}
+	  });
+	
+	
+	  
+
+	
 }
 
-function closeAbout() {
-	document.getElementById("About").close();
-}
+// function closeAbout() {
+// 	document.getElementById("About").close();
+// }
 
 function openConclude() {
 	document.getElementById("conclude").showModal();
